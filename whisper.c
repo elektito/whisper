c81102dccd34f96034d22e07428a8ec35fe37760
Mostafa Razavi <mostafa@sepent.com>
2024-11-08 21:05:11 +0100

    Add "error" form

--- whisper.c
+++ whisper.c
@@ -1528,6 +1528,25 @@ compile_include(struct function *func, int indent, struct value *form)
 }
 
 int
+compile_error(struct function *func, int indent, struct value *form)
+{
+    if (form->list.length != 2) {
+        fprintf(stderr, "malformed error\n");
+        exit(1);
+    }
+
+    int msg_varnum = compile_form(func, indent, &form->list.ptr[1]);
+    gen_code(func, indent, "printf(\"error: \");\n");
+    gen_code(func, indent, "display(x%d);\n", msg_varnum);
+    gen_code(func, indent, "printf(\"\\n\");\n");
+
+    int ret_varnum = func->varnum++;
+    gen_code(func, indent, "value x%d = VOID;\n", ret_varnum);
+
+    return ret_varnum;
+}
+
+int
 compile_quoted_item(struct function *func, int indent, struct value *form)
 {
     int varnum;
@@ -1912,6 +1931,11 @@ compile_list(struct function *func, int indent, struct value *form)
                memcmp(list_car->identifier.name, "include", 7) == 0)
     {
         varnum = compile_include(func, indent, form);
+    } else if (list_car->type == VAL_ID &&
+               list_car->identifier.name_len == 5 &&
+               memcmp(list_car->identifier.name, "error", 5) == 0)
+    {
+        varnum = compile_error(func, indent, form);
     } else {
         varnum = compile_call(func, indent, form);
     }
