e3c1f75945cf06dec0e7046b60c81d4aa167501b
Mostafa Razavi <mostafa@sepent.com>
2026-06-15 12:31:38 +0330

    Add libwhisper.a
    The compiler's main code is now compiled into a static libwhisper.a
    library. In order to do this, we refactored the compiler code and moved
    any cli related code to main.scm. The compiler executable is built from
    main.scm (which includes whisper.scm itself). The library is built from
    just libwhisper.scm.

--- Makefile
+++ Makefile
@@ -1,32 +1,35 @@
 CFLAGS ?=
 
-SRC_FILES = whisper.scm utils.scm format.scm qq.scm macro.scm preprocess.scm
+SRC_FILES = whisper.scm utils.scm format.scm qq.scm macro.scm preprocess.scm main.scm
 
 all: whisper-v10
 
 stage0: $(SRC_FILES)
-	./whisper-v9 whisper.scm -o stage0 -C prev
+	./whisper-v9 main.scm -o stage0 -C prev
 
 stage1: stage0 core.h core.c $(SRC_FILES)
-	./stage0 whisper.scm -o stage1 -f "-Wl,-s $(CFLAGS)"
+	./stage0 main.scm -o stage1 -f "-Wl,-s $(CFLAGS)"
 
 whisper-v10: stage1 core.h core.c $(SRC_FILES)
-	./stage1 whisper.scm -o whisper-v10 -f "-Wl,-s $(CFLAGS)"
+	./stage1 main.scm -o whisper-v10 -f "-Wl,-s $(CFLAGS)"
 	diff stage1 whisper-v10
 
 test: whisper-v10
 	./whisper-v10 test.scm -t -r
 
 matrix: whisper-v10
-	./whisper-v10 whisper.scm -c -o /tmp/b.c
+	./whisper-v10 main.scm -c -o /tmp/b.c
 	@for o in 0 1 2 3; do \
 		echo "--- O$$o ---"; \
 		gcc -O$$o -Wl,-s -I. -o /tmp/b.$$o /tmp/b.c core.c \
-		&& /tmp/b.$$o whisper.scm -o /tmp/out.$$o \
+		&& /tmp/b.$$o main.scm -o /tmp/out.$$o \
 		&& /tmp/b.$$o test.scm -t -r || exit 1; \
 	done
 
+libwhisper.a: whisper-v10 $(SRC_FILES) core.h core.c
+	./whisper-v10 whisper.scm -L -o libwhisper.a
+
 clean:
-	rm -f whisper-v10 stage0 stage1
+	rm -f whisper-v10 stage0 stage1 libwhisper.a
 
 .PHONY: all clean test matrix
---
+++ main.scm
@@ -0,0 +1,209 @@
+(include "whisper.scm")
+
+(define (hex-encode s)
+  (let loop ((i 0) (x ""))
+    (if (= i (string-length s))
+        x
+        (let ((c (char->integer (string-ref s i))))
+          (if (< c 16)
+              (loop (+ i 1) (format "~a0~x" x c))
+              (loop (+ i 1) (format "~a~x" x c)))))))
+
+(define (temp-filename)
+  (format "/tmp/~a" (hex-encode (urandom 6))))
+
+(define (all-archive-flags archives)
+  (if (null? archives)
+      ""
+      (string-append "-Wl,--whole-archive " (string-join archives " ") " -Wl,--no-whole-archive")))
+
+(define (build-compile-cmd cc own-cflags args c-file)
+  (let ((out (cmdline-executable-file args))
+        (extra (cmdline-cflags args))
+        (core-path (cmdline-core-path args))
+        (archive-flags (all-archive-flags (cmdline-archives args))))
+    (case (cmdline-library-mode args)
+      ((so)
+       (format "~a -I~a ~a -fPIC -shared -o ~a ~a ~a" cc core-path own-cflags out extra c-file))
+      ((static)
+       (let ((obj (string-append (temp-filename) ".o")))
+         (format "~a -I~a ~a -fPIC -c -o ~a ~a ~a && rm -f ~a && ar rcs ~a ~a" cc core-path own-cflags obj extra c-file out out obj)))
+      (else
+       (format "~a -I~a ~a -Wl,--export-dynamic -o ~a ~a ~a ~a ~a/core.c" cc core-path own-cflags out extra c-file archive-flags core-path)))))
+
+;;;;;; command-line parsing ;;;;;;
+
+(define-record-type <cmdline>
+  (make-cmdline just-compile run output-file input-file test c-file executable-file delete-executable debug cflags library-mode core-path archives)
+  cmdline?
+  (just-compile cmdline-just-compile cmdline-just-compile-set!)
+  (run cmdline-run cmdline-run-set!)
+  (output-file cmdline-output-file cmdline-output-file-set!)
+  (input-file cmdline-input-file cmdline-input-file-set!)
+  (test cmdline-test cmdline-test-set!)
+  (c-file cmdline-c-file cmdline-c-file-set!)
+  (executable-file cmdline-executable-file cmdline-executable-file-set!)
+  (delete-executable cmdline-delete-executable cmdline-delete-executable-set!)
+  (debug cmdline-debug cmdline-debug-set!)
+  (cflags cmdline-cflags cmdline-cflags-set!)
+  (library-mode cmdline-library-mode cmdline-library-mode-set!)
+  (core-path cmdline-core-path cmdline-core-path-set!)
+  (archives cmdline-archives cmdline-archives-set!))
+
+(define (create-cmdline-args)
+  (make-cmdline #f  ; just compile
+                #f  ; run
+                #f  ; output file
+                #f  ; input file
+                #f  ; test
+                #f  ; c output file
+                #f  ; executable output file
+                #f  ; delete executable
+                #f  ; debug
+                ""  ; cflags
+                #f  ; library mode
+                "." ; core path
+                '() ; archives
+                ))
+
+(define (command-line-error fmt . args)
+  (apply format (current-error-port) fmt args)
+  (newline (current-error-port))
+  (exit 1))
+
+(define (parse-command-line-args)
+  (let ((args (create-cmdline-args)))
+    (let loop ((cl (cdr (command-line))))
+      (cond ((null? cl) args)
+            ((string=? (car cl) "-h")
+             (print-usage))
+            ((string=? (car cl) "-?")
+             (print-usage))
+            ((string=? (car cl) "-c")
+             (cmdline-just-compile-set! args #t)
+             (loop (cdr cl)))
+            ((string=? (car cl) "-r")
+             (cmdline-run-set! args #t)
+             (loop (cdr cl)))
+            ((string=? (car cl) "-t")
+             (cmdline-test-set! args #t)
+             (loop (cdr cl)))
+            ((string=? (car cl) "-g")
+             (cmdline-debug-set! args #t)
+             (loop (cdr cl)))
+            ((string=? (car cl) "-l")
+             (cmdline-library-mode-set! args 'so)
+             (loop (cdr cl)))
+            ((string=? (car cl) "-L")
+             (cmdline-library-mode-set! args 'static)
+             (loop (cdr cl)))
+            ((string=? (car cl) "-C")
+             (if (null? (cdr cl))
+                 (command-line-error "missing argument to -C")
+                 (begin
+                   (cmdline-core-path-set! args (cadr cl))
+                   (loop (cddr cl)))))
+            ((string=? (car cl) "-a")
+             (if (null? (cdr cl))
+                 (command-line-error "missing argument to -a")
+                 (begin
+                   (cmdline-archives-set! args (cons (cadr cl) (cmdline-archives args)))
+                   (loop (cddr cl)))))
+            ((string=? (car cl) "-f")
+             (if (null? (cdr cl))
+                 (command-line-error "missing argument to -f")
+                 (begin
+                   (cmdline-cflags-set! args (cadr cl))
+                   (loop (cddr cl)))))
+            ((string=? (car cl) "-o")
+             (if (null? (cdr cl))
+                 (command-line-error "missing argument to -o")
+                 (begin
+                   (cmdline-output-file-set! args (cadr cl))
+                   (loop (cddr cl)))))
+            (else (if (cmdline-input-file args)
+                      (command-line-error "unexpected argument: ~a" (car cl))
+                      (begin
+                        (cmdline-input-file-set! args (car cl))
+                        (loop (cdr cl)))))))))
+
+(define (postprocess-cmdline args)
+  (if (not (cmdline-input-file args))
+      (command-line-error "missing input file"))
+  (if (and (cmdline-just-compile args) (cmdline-run args))
+      (command-line-error "-r and -c are mutually exclusive"))
+  (if (and (cmdline-library-mode args) (cmdline-run args))
+      (command-line-error "-l/-L and -r are mutually exclusive"))
+  (if (cmdline-output-file args)
+      (if (cmdline-just-compile args)
+          (cmdline-c-file-set! args (cmdline-output-file args))
+          (begin
+            (cmdline-c-file-set! args (string-append (temp-filename) ".c"))
+            (cmdline-executable-file-set! args (cmdline-output-file args))))
+      (begin
+        (cmdline-delete-executable-set! args #t)
+        (if (cmdline-just-compile args)
+            (cmdline-output-file-set! args "b.c")
+            (if (cmdline-run args)
+                (begin
+                  (cmdline-output-file-set! args (temp-filename))
+                  (cmdline-delete-executable-set! args #t))
+                (case (cmdline-library-mode args)
+                  ((so)     (cmdline-output-file-set! args "b.so"))
+                  ((static) (cmdline-output-file-set! args "b.a"))
+                  (else     (cmdline-output-file-set! args "b.out")))))))
+  (if (cmdline-just-compile args)
+      (cmdline-c-file-set! args (cmdline-output-file args))
+      (begin
+        (cmdline-c-file-set! args (string-append (temp-filename) ".c"))
+        (cmdline-executable-file-set! args (cmdline-output-file args)))))
+
+(define (print-usage)
+  (format (current-error-port) "usage: ~a input-file [-r] [-c] [-l] [-L] [-C core-path] [-o output-file] [-f cflags] [-a archive] [-g]
+
+ -r\tcompile and run the program
+ -c\tonly compile a c file
+ -l\tcompile as a shared library (.so)
+ -L\tcompile as a static library (.a)
+ -C\tpath to core files (core.h and core.c).
+ -o\tthe name of the output file. defaults to b.c, b.out, b.so, or b.a
+\tdepending on -c, -l, -L, or neither.
+ -f\tuse the given options when invoking the C compiler
+ -a\tlink a static archive (may be repeated)
+ -t\tcompile the program as a test suite
+ -g\tadd debug instrumentation
+" (car (command-line)))
+  (exit 0)
+  )
+
+;;;;;; main ;;;;;;
+
+(let ((args (parse-command-line-args)))
+  (postprocess-cmdline args)
+  (let ((port (open-input-file (cmdline-input-file args))))
+    (let ((program (create-program port)))
+      (if (cmdline-test args)
+          (program-is-test-suite-set! program #t))
+      (if (cmdline-debug args)
+          (program-debug-set! program #t))
+      (when (cmdline-library-mode args)
+        (program-library-mode-set! program (cmdline-library-mode args)))
+      (compile-program program)
+      (output-program-code program (cmdline-c-file args))
+      (if (not (cmdline-just-compile args))
+          (let ((cc (get-environment-variable "CC")))
+            (let ((cc (if cc cc "gcc"))
+                  (own-cflags (if (program-debug program) " -DDEBUG" "")))
+              (let ((cmd (build-compile-cmd cc own-cflags args (cmdline-c-file args))))
+                (let ((ret (system cmd)))
+                  (delete-file (cmdline-c-file args))
+                  (if (not (zero? ret))
+                      (begin
+                        (format (current-error-port) "program exited with error code: ~a\n" ret)
+                        (exit ret))))))))
+      (if (cmdline-run args)
+          (let ((cmd (format "$(realpath ~a)" (cmdline-executable-file args))))
+            (let ((ret (system cmd)))
+              (if (cmdline-delete-executable args)
+                  (delete-file (cmdline-executable-file args)))
+              (exit ret)))))))
