1fc38fd80598300d8260562e244928ec473f645e
Mostafa Razavi <mostafa@sepent.com>
2026-08-05 13:54:56 +0330

    Refactor bootstrap.sh
    Instead of lots of duplicated code, just use a for loop!

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,166 +2,33 @@
 
 set -e
 
+CURRENT_V=23
+LAST_V=$((CURRENT_V - 1))
+
 mkdir -p bootstrap
 cd bootstrap
 rm -rf *
 
 echo "==== v1 ===="
-git clone .. --branch=v1 v1 >/dev/null >/dev/null 2>&1
+git clone .. --branch=v1 v1 >/dev/null 2>&1
 cd v1
 make
-
-echo "==== v2 ===="
 cd ..
-git clone .. --branch=v2 v2 >/dev/null >/dev/null 2>&1
-cd v2
-cp ../v1/whisper-v1 .
-make
 
-echo "==== v3 ===="
-cd ..
-git clone .. --branch=v3 v3 >/dev/null >/dev/null 2>&1
-cd v3
-cp ../v2/whisper-v2 .
-make
-
-echo "==== v4 ===="
-cd ..
-git clone .. --branch=v4 v4 >/dev/null >/dev/null 2>&1
-cd v4
-cp ../v3/whisper-v3 .
-make
-
-echo "==== v5 ===="
-cd ..
-git clone .. --branch=v5 v5 >/dev/null 2>&1
-cd v5
-cp ../v4/whisper-v4 .
-make
-
-echo "==== v6 ===="
-cd ..
-git clone .. --branch=v6 v6 >/dev/null 2>&1
-cd v6
-cp ../v5/whisper-v5 .
-make
-
-echo "==== v7 ===="
-cd ..
-git clone .. --branch=v7 v7 >/dev/null 2>&1
-cd v7
-cp ../v6/whisper-v6 .
-make
-
-echo "==== v8 ===="
-cd ..
-git clone .. --branch=v8 v8 >/dev/null 2>&1
-cd v8
-cp ../v7/whisper-v7 .
-make
-
-echo "==== v9 ===="
-cd ..
-git clone .. --branch=v9 v9 >/dev/null 2>&1
-cd v9
-cp ../v8/whisper-v8 .
-make
-
-echo "==== v10 ===="
-cd ..
-git clone .. --branch=v10 v10 >/dev/null 2>&1
-cd v10
-cp ../v9/whisper-v9 .
-make
-
-echo "==== v11 ===="
-cd ..
-git clone .. --branch=v11 v11 >/dev/null 2>&1
-cd v11
-cp ../v10/whisper-v10 .
-make
-
-echo "==== v12 ===="
-cd ..
-git clone .. --branch=v12 v12 >/dev/null 2>&1
-cd v12
-cp ../v11/whisper-v11 .
-make
-
-echo "==== v13 ===="
-cd ..
-git clone .. --branch=v13 v13 >/dev/null 2>&1
-cd v13
-cp ../v12/whisper-v12 .
-make
-
-echo "==== v14 ===="
-cd ..
-git clone .. --branch=v14 v14 >/dev/null 2>&1
-cd v14
-cp ../v13/whisper-v13 .
-make
-
-echo "==== v15 ===="
-cd ..
-git clone .. --branch=v15 v15 >/dev/null 2>&1
-cd v15
-cp ../v14/whisper-v14 .
-make
-
-echo "==== v16 ===="
-cd ..
-git clone .. --branch=v16 v16 >/dev/null 2>&1
-cd v16
-cp ../v15/whisper-v15 .
-make
-
-echo "==== v17 ===="
-cd ..
-git clone .. --branch=v17 v17 >/dev/null 2>&1
-cd v17
-cp ../v16/whisper-v16 .
-make
-
-echo "==== v18 ===="
-cd ..
-git clone .. --branch=v18 v18 >/dev/null 2>&1
-cd v18
-cp ../v17/whisper-v17 .
-make
-
-echo "==== v19 ===="
-cd ..
-git clone .. --branch=v19 v19 >/dev/null 2>&1
-cd v19
-cp ../v18/whisper-v18 .
-make
-
-echo "==== v20 ===="
-cd ..
-git clone .. --branch=v20 v20 >/dev/null 2>&1
-cd v20
-cp ../v19/whisper-v19 .
-make
-
-echo "==== v21 ===="
-cd ..
-git clone .. --branch=v21 v21 >/dev/null 2>&1
-cd v21
-cp ../v20/whisper-v20 .
-make
-
-echo "==== v22 ===="
-cd ..
-git clone .. --branch=v22 v22 >/dev/null 2>&1
-cd v22
-cp ../v21/whisper-v21 .
-make
+for i in $(seq 2 "$LAST_V"); do
+  prev=$((i - 1))
+  echo "==== v$i ===="
+  git clone .. --branch=v$i v$i >/dev/null 2>&1
+  cd v$i
+  cp ../v$prev/whisper-v$prev .
+  make
+  cd ..
+done
 
 echo "==== current ===="
-cd ..
 git clone .. --branch=master current >/dev/null 2>&1
 cd current
-cp ../v22/whisper-v22 .
+cp ../v$LAST_V/whisper-v$LAST_V .
 make
-cp whisper-v23 ../whisper
+cp whisper-v$CURRENT_V ../whisper
+cd ..
