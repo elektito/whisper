a8332f2babf9da0ecb8f257a8b838b60b9fb4cdf
Mostafa Razavi <mostafa@sepent.com>
2026-09-06 14:47:28 +0330

    Add more bindings to raylib library

--- lib/raylib.c
+++ lib/raylib.c
@@ -2,6 +2,41 @@
 
 #include <raylib.h>
 
+Color color_from_list(value color_scm, const char *caller_name) {
+    const char *error_msg = "color must be a list of four values (r g b a), each from in range [0, 255]";
+    if (!IS_PAIR(color_scm)) { raise_error("%s: %s", caller_name, error_msg); }
+    struct pair *first_pair = GET_PAIR(color_scm);
+    if (!IS_PAIR(first_pair->cdr)) { raise_error("%s: %s", caller_name, error_msg); }
+    struct pair *second_pair = GET_PAIR(first_pair->cdr);
+    if (!IS_PAIR(second_pair->cdr)) { raise_error("%s: %s", caller_name, error_msg); }
+    struct pair *third_pair = GET_PAIR(second_pair->cdr);
+    if (!IS_PAIR(third_pair->cdr)) { raise_error("%s: %s", caller_name, error_msg); }
+    struct pair *fourth_pair = GET_PAIR(third_pair->cdr);
+
+    if (!IS_FIXNUM(first_pair->car)) { raise_error("%s: %s", caller_name, error_msg); }
+    if (!IS_FIXNUM(second_pair->car)) { raise_error("%s: %s", caller_name, error_msg); }
+    if (!IS_FIXNUM(third_pair->car)) { raise_error("%s: %s", caller_name, error_msg); }
+    if (!IS_FIXNUM(fourth_pair->car)) { raise_error("%s: %s", caller_name, error_msg); }
+
+    int r = GET_FIXNUM(first_pair->car);
+    int g = GET_FIXNUM(second_pair->car);
+    int b = GET_FIXNUM(third_pair->car);
+    int a = GET_FIXNUM(fourth_pair->car);
+
+    if (r < 0 || r > 255) { raise_error("%s: %s", caller_name, error_msg); }
+    if (g < 0 || g > 255) { raise_error("%s: %s", caller_name, error_msg); }
+    if (b < 0 || b > 255) { raise_error("%s: %s", caller_name, error_msg); }
+    if (a < 0 || a > 255) { raise_error("%s: %s", caller_name, error_msg); }
+
+    Color color;
+    color.r = r;
+    color.g = g;
+    color.b = b;
+    color.a = a;
+
+    return color;
+}
+
 value init_window(environment env, enum call_flags flags, int nargs, ...) {
     if (nargs != 3) { raise_error("init-window needs three arguments"); }
     init_args();
@@ -20,5 +55,41 @@ value init_window(environment env, enum call_flags flags, int nargs, ...) {
 
     InitWindow(GET_FIXNUM(width), GET_FIXNUM(height), title_c);
 
+    return VOID;
+}
+
+value close_window(environment env, enum call_flags flags, int nargs, ...) {
+    if (nargs != 0) { raise_error("close-window accepts no arguments"); }
+    CloseWindow();
+    return VOID;
+}
+
+value window_should_close(environment env, enum call_flags flags, int nargs, ...) {
+    if (nargs != 0) { raise_error("window-should-close accepts no arguments"); }
+    return BOOL(WindowShouldClose());
+}
+
+value clear_background(environment env, enum call_flags flags, int nargs, ...) {
+    if (nargs != 1) { raise_error("clear-background takes a single argument"); }
+
+    init_args();
+    value color_scm = next_arg();
+    free_args();
+
+    Color color = color_from_list(color_scm, "clear-background");
+
+    ClearBackground(color);
+    return VOID;
+}
+
+value begin_drawing(environment env, enum call_flags flags, int nargs, ...) {
+    if (nargs != 0) { raise_error("begin-drawing accepts no arguments"); }
+    BeginDrawing();
+    return VOID;
+}
+
+value end_drawing(environment env, enum call_flags flags, int nargs, ...) {
+    if (nargs != 0) { raise_error("end-drawing accepts no arguments"); }
+    EndDrawing();
     return VOID;
 }
--- lib/raylib.sld
+++ lib/raylib.sld
@@ -1,6 +1,71 @@
 (define-library (raylib)
+  (import (whisper core))
+
+  (begin
+    (define light-gray '(200 200 200 255))
+    (define gray '(130 130 130 255))
+    (define dark-gray '(80 80 80 255))
+    (define yellow '(253 249 0 255))
+    (define gold '(255 203 0 55))
+    (define orange '(255 161 0 255))
+    (define pink '(255 109 194 255))
+    (define red '(230 41 55 255))
+    (define maroon '(190 33 55 255))
+    (define gree '(0 228 48 255))
+    (define lime '(0 158 47 255))
+    (define dark-green '(0 117 44 255))
+    (define sky-blue '(102 191 255 255))
+    (define blue '(0 121 241 255))
+    (define dark-blue '(0 82 172 255))
+    (define purple '(200 122 255 255))
+    (define violet '(135 60 190 255))
+    (define dark-purple '(112 31 126 255))
+    (define beige '(211 176 131 255))
+    (define brown  '(127 106 79 255))
+    (define dark-brown '(76 63 47 255))
+
+    (define white '(255 255 255 255))
+    (define black '(0 0 0 255))
+    (define blank '(0 0 0 0))
+    (define magenta '(255 0 255 255))
+    (define ray-white '(245 245 245 255)))
+
+  (export light-gray
+          gray
+          dark-gray
+          yellow
+          gold
+          orange
+          pink
+          red
+          maroon
+          gree
+          lime
+          dark-green
+          sky-blue
+          blue
+          dark-blue
+          purple
+          violet
+          dark-purple
+          beige
+          brown
+          dark-brown
+
+          white
+          black
+          blank
+          magenta
+          ray-white)
+
   (c-include "raylib.c")
-  (c-export (init-window "init_window" 3 3))
+  (c-export (init-window "init_window" 3 3)
+            (close-window "close_window" 0 0)
+            (window-should-close "window_should_close" 0 0)
+
+            (clear-background "clear_background" 1 1)
+            (begin-drawing "begin_drawing" 0 0)
+            (end-drawing "end_drawing" 0 0))
   (c-archives "libraylib.a")
   (c-static-flags "-lm -lX11 -lGL")
   (c-so-flags "-lraylib -Wl,-rpath '$ORIGIN' -lm -lX11 -lGL"))
